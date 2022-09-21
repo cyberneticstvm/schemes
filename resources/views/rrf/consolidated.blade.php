b7566517e33cff458a08af8c27720ad2d3c5323c
Vijoy Sasidharan <devuinfosys@gmail.com>
2022-09-21 21:40:39 +0530

    small updates

--- resources/views/rrf/consolidated.blade.php
+++ resources/views/rrf/consolidated.blade.php
@@ -46,7 +46,7 @@
                                 <table class="table table-bordered table-sm table-striped table-hover">
                                     <thead>
                                         <tr><th></th><th colspan="4" class="text-center">Corporations</th><th colspan="4" class="text-center">Municipalities</th><th colspan="4" class="text-center">Grama Panchayats</th><th colspan="4" class="text-center">Block Panachayats</th></tr>
-                                        <tr><th>District</th><td>No.of Corp.</td><td>RRF's Rqd</td><td>RRF's Functional</td><td>%</td><td>No.of Municipals.</td><td>RRF's Rqd</td><td>RRF's Functional</td><td>%</td><td>No.of Panchayats.</td><td>RRF's Rqd</td><td>RRF's Functional</td><td>%</td><td>No.of Blocks</td><td>RRF's Rqd</td><td>RRF's Functional</td><td>%</td></tr>
+                                        <tr><th>District</th><td>No.of Corp.</td><td>RRF's Rqd</td><td>RRF's Functional</td><td>%</td><td>No.of Municipals.</td><td>RRF's Rqd</td><td>RRF's Functional</td><td>%</td><td>No.of Panchayats.</td><!--<td>RRF's Rqd</td>--><td>RRF's Functional</td><!--<td>%</td>--><td>No.of Blocks</td><td>RRF's Rqd</td><td>RRF's Functional</td><td>%</td></tr>
                                     </thead>
                                     <tbody>
                                         @php $c = 1; $a= 0; $b = 0; $c = 0; $d = 0; $e = 0; $e1 = 0; $f = 0; $g = 0; $h = 0; $i = 0; $i1 = 0; $j = 0; $k = 0; $l = 0; $m = 0; $n = 0; $o = 0; $p = 0; $p1 = 0; $q = 0; $r = 0; @endphp
@@ -73,9 +73,9 @@
                                                 <td class="text-right">{{ ($rrf_mp_reqd > 0 && $rrf_mp > 0) ? number_format((100/$rrf_mp_reqd)*$rrf_mp, 2) : 0.00 }}%</td>
 
                                                 <td class="text-right">{{ $i1 = DB::table('gramapanchayats as g')->leftJoin('districts as d', 'g.district', 'd.id')->where('g.district', $row->id)->count('g.id') }}</td>
-                                                <td class="text-right">{{ $rrf_gp_reqd }}</td>
+                                                <!--<td class="text-right">{{ $rrf_gp_reqd }}</td>-->
                                                 <td class="text-right">{{ $rrf_gp }}</td>
-                                                <td class="text-right">{{ ($rrf_gp_reqd > 0 && $rrf_gp > 0) ? number_format((100/$rrf_gp_reqd)*$rrf_gp, 2) : 0.00 }}%</td>
+                                                <!--<td class="text-right">{{ ($rrf_gp_reqd > 0 && $rrf_gp > 0) ? number_format((100/$rrf_gp_reqd)*$rrf_gp, 2) : 0.00 }}%</td>-->
 
                                                 <td class="text-right">{{ $p1 = DB::table('blocks as b')->leftJoin('districts as d', 'b.district', 'd.id')->where('b.district', $row->id)->count('b.id') }}</td>
                                                 <td class="text-right">{{ $rrf_bl_reqd }}</td>
@@ -99,7 +99,7 @@
                                             @endphp
                                         @empty
                                         @endforelse
-                                        <tr><td class="font-weight-bold text-right">Total</td><td class="font-weight-bold text-right">{{ $a }}</td><td class="font-weight-bold text-right">{{ $b }}</td><td class="font-weight-bold text-right">{{ $c }}</td><td class="font-weight-bold text-right">{{ number_format((100/$b)*$c, 2) }}%</td><td class="font-weight-bold text-right">{{ $e }}</td><td class="font-weight-bold text-right">{{ $f }}</td><td class="font-weight-bold text-right">{{ $g }}</td><td class="font-weight-bold text-right">{{ number_format((100/$f)*$g, 2) }}%</td><td class="font-weight-bold text-right">{{ $i }}</td><td class="font-weight-bold text-right">{{ $j }}</td><td class="font-weight-bold text-right">{{ $k }}</td><td class="font-weight-bold text-right">{{ number_format((100/$j)*$k, 2) }}%</td><td class="font-weight-bold text-right">{{ $p }}</td><td class="font-weight-bold text-right">{{ $q }}</td><td class="font-weight-bold text-right">{{ $r }}</td><td class="font-weight-bold text-right">{{ number_format((100/$q)*$r, 2) }}%</td></tr>
+                                        <tr><td class="font-weight-bold text-right">Total</td><td class="font-weight-bold text-right">{{ $a }}</td><td class="font-weight-bold text-right">{{ $b }}</td><td class="font-weight-bold text-right">{{ $c }}</td><td class="font-weight-bold text-right">{{ number_format((100/$b)*$c, 2) }}%</td><td class="font-weight-bold text-right">{{ $e }}</td><td class="font-weight-bold text-right">{{ $f }}</td><td class="font-weight-bold text-right">{{ $g }}</td><td class="font-weight-bold text-right">{{ number_format((100/$f)*$g, 2) }}%</td><td class="font-weight-bold text-right">{{ $i }}</td><!--<td class="font-weight-bold text-right">{{ $j }}</td>--><td class="font-weight-bold text-right">{{ $k }}</td><!--<td class="font-weight-bold text-right">{{ number_format((100/$j)*$k, 2) }}%</td>--><td class="font-weight-bold text-right">{{ $p }}</td><td class="font-weight-bold text-right">{{ $q }}</td><td class="font-weight-bold text-right">{{ $r }}</td><td class="font-weight-bold text-right">{{ number_format((100/$q)*$r, 2) }}%</td></tr>
                                     </tbody>
                                 </table>
                             </div>
